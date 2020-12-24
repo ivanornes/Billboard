719b7476a7b6e6f5e59d7251ac5adec7518cc442
Ivan <ivan.ornes@gmail.com>
2020-12-24 16:06:35 +0100

    Initial Commit

---
+++ Billboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Billboard
+//
+//  Created by Ivan Ornes on 24/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
